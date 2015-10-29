ac9cd7f1dd9a3a7327966046a741efff2646040a
zenithit <zenith.it.ju@gmail.com>
2015-10-29 15:12:57 +0600

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
 
   def index
     @users = User.paginate(page: params[:page])
